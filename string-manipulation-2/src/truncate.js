e2b73b8e0d49f1b506064ff6d8a7f3ab0d80c8c9
Tyler Stahl <tstahl488@gmail.com>
2023-09-01 21:07:14 +0000

    incomplete string-manipulation-2

--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -1,9 +1,31 @@
 /* exported truncate */
+
+// function truncate(length, string) {
+//   let newString = '';
+//   for (let i = 0; i < length; i++) {
+//     if (string !== ''){
+//     newString += string[i];
+//     } else {
+
+//     }
+//   }
+//   newString += '...';
+//   return newString;
+// }
+// console.log(truncate(8, 'This string is way too long'))
+// console.log(truncate(5, ''))
+
 function truncate(length, string) {
   let newString = '';
-  for (let i = 0; i < length; i++) {
-    newString += i;
+  if (string !== '') {
+    for (let i = 0; i < length; i++) {
+      newString += string[i];
+    }
+  } else {
+    newString = '';
   }
   newString += '...';
   return newString;
 }
+console.log(truncate(8, 'This string is way too long'));
+console.log(truncate(5, ''));
