65077484c2ff99d863a9f33efb68bc0a02438ff3
Tyler Stahl <tstahl488@gmail.com>
2023-09-01 21:44:58 +0000

    incomplete string-manipulation-2

--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -17,6 +17,9 @@
 
 function truncate(length, string) {
   let newString = '';
+  if (length > string.length) {
+    length = string.length;
+  }
   if (string !== '') {
     for (let i = 0; i < length; i++) {
       newString += string[i];
@@ -27,5 +30,7 @@ function truncate(length, string) {
   newString += '...';
   return newString;
 }
+
 console.log(truncate(8, 'This string is way too long'));
 console.log(truncate(5, ''));
+console.log(truncate(20, 'LearningFuze'));
