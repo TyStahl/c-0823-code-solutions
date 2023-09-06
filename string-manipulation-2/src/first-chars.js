2735293b3b71d7b423bf613d097a31490b6dde4c
Tyler Stahl <tstahl488@gmail.com>
2023-09-06 16:14:38 +0000

    incomplete string-manipulation-2

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,22 @@
 /* exported firstChars */
+function firstChars(length, string) {
+  let newString = '';
+  if (length > string.length) {
+    length = string.length;
+  }
+  if (string !== '') {
+    for (let i = 0; i < length; i++) {
+      newString += string[i];
+    }
+  } else {
+    newString = '';
+  }
+  return newString;
+}
+
+console.log(firstChars(8, 'All Code All Day'));
+console.log(firstChars(15, 'HTML, CSS JavaScript, React'));
+console.log(firstChars(1, 'React'));
+console.log(firstChars(3, 'Angular'));
+console.log(firstChars(5, ''));
+console.log(firstChars(20, 'LearningFuze'));
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,32 @@
 /* exported numVowels */
+function numVowels(string) {
+  const lowerCaseString = string.toLowerCase();
+  let finalString = '';
+  for (let i = 0; i < lowerCaseString.length; i++) {
+    switch (lowerCaseString[i]) {
+      case 'a':
+        finalString += lowerCaseString[i];
+        break;
+      case 'e':
+        finalString += lowerCaseString[i];
+        break;
+      case 'i':
+        finalString += lowerCaseString[i];
+        break;
+      case 'o':
+        finalString += lowerCaseString[i];
+        break;
+      case 'u':
+        finalString += lowerCaseString[i];
+        break;
+    }
+  }
+  return finalString.length;
+}
+
+console.log(numVowels('All Code All Day'));
+console.log(numVowels('HTML, CSS, JavaScript, React'));
+console.log(numVowels('React'));
+console.log(numVowels('Angular'));
+console.log(numVowels(''));
+console.log(numVowels('LearningFuze'));
