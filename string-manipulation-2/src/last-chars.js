dea51504e3d4a7acf41be7ffe4d7d50f7c043275
Tyler Stahl <tstahl488@gmail.com>
2023-09-06 18:31:26 +0000

    lastchars progress

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,13 @@
 /* exported lastChars */
+function lastChars(length, string) {
+  let newString = '';
+  const lastOf = string.length - length;
+  for (let i = string.length; i > lastOf; i--) {
+    newString += string[i];
+  }
+
+  return newString;
+}
+
+console.log('----lastChars----');
+console.log(lastChars(8, 'All Code All Day'));
