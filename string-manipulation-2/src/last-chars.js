743e402ae3f9e2edef5613e86636a9fa6ca441b0
Tyler Stahl <tstahl488@gmail.com>
2023-09-06 22:19:09 +0000

    incomplete string-manipulation-2

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,13 +1,17 @@
 /* exported lastChars */
 function lastChars(length, string) {
   let newString = '';
-  const lastOf = string.length - length;
-  for (let i = string.length; i > lastOf; i--) {
-    newString += string[i];
+  if (string.length < length) {
+    length = string.length;
   }
-
+  newString = string.slice(string.length - length);
   return newString;
 }
 
 console.log('----lastChars----');
 console.log(lastChars(8, 'All Code All Day'));
+console.log(lastChars(15, 'HTML, CSS, JavaScript, React'));
+console.log(lastChars(1, 'React'));
+console.log(lastChars(3, 'Angular'));
+console.log(lastChars(5, ''));
+console.log(lastChars(20, 'LearningFuze'));
--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -1,19 +1,8 @@
 /* exported truncate */
 
-// function truncate(length, string) {
-//   let newString = '';
-//   for (let i = 0; i < length; i++) {
-//     if (string !== ''){
-//     newString += string[i];
-//     } else {
-
-//     }
-//   }
-//   newString += '...';
-//   return newString;
+// function truncate(length, string){
+//   return string.slice(0, length) + '...';
 // }
-// console.log(truncate(8, 'This string is way too long'))
-// console.log(truncate(5, ''))
 
 function truncate(length, string) {
   let newString = '';
