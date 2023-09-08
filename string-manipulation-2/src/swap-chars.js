718d6eaa5f5c436d56a03a80f5dad671d03db8eb
Tyler Stahl <tstahl488@gmail.com>
2023-09-08 17:37:25 +0000

    incomplete

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,8 +1,1 @@
 /* exported swapChars */
-function swapChars(firstIndex, secondIndex, string) {
-  string.replace(string[firstIndex], string[secondIndex]);
-  string.replace(string[secondIndex], string[firstIndex]);
-  return string;
-}
-console.log('----swapChars-----');
-console.log(swapChars('oldash'));
