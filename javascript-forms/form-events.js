9c89e14bc8219ab8faf87171993045e998d9c70c
Tyler Stahl <tstahl488@gmail.com>
2023-09-13 18:51:56 +0000

    fixed forms-events.js file

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -7,17 +7,23 @@ function handleFocus(event) {
   console.log(event.target.name);
 }
 $userName.addEventListener('focus', handleFocus);
+$userName.addEventListener('blur', handleBlur);
+$userName.addEventListener('input', handleInput);
 
 function handleBlur(event) {
   console.log('blur');
   console.log(event.target.name);
 }
 
+$userEmail.addEventListener('focus', handleFocus);
 $userEmail.addEventListener('blur', handleBlur);
+$userEmail.addEventListener('input', handleInput);
 
 function handleInput(event) {
   console.log(event.target.name);
   console.log(event.target.value);
 }
 
+$userMessage.addEventListener('focus', handleFocus);
+$userMessage.addEventListener('blur', handleBlur);
 $userMessage.addEventListener('input', handleInput);
