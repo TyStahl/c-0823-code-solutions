d46bb8b9ad47aa770b69668863f0a9f03c03a8dd
Tyler Stahl <tstahl488@gmail.com>
2023-09-14 23:43:40 +0000

    added $ to dom variable

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -6,11 +6,11 @@ function handleClick(event) {
   console.log('the event target it', event.target);
   console.log('the event targets tag name is', event.target.tagName);
 
-  const nearElement = event.target.closest('.task-list-item');
+  const $nearElement = event.target.closest('.task-list-item');
 
   if (event.target.matches('button')) {
-    console.log('closest task-list-item ancestor element is the', nearElement);
+    console.log('closest task-list-item ancestor element is the', $nearElement);
 
-    nearElement.remove();
+    $nearElement.remove();
   }
 }
