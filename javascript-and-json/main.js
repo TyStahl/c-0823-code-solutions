784b7788716b7f79e23bef02e8a940551bca0657
Tyler Stahl <tstahl488@gmail.com>
2023-09-15 18:00:51 +0000

    javasript-and-json start

---
+++ javascript-and-json/main.js
@@ -0,0 +1,18 @@
+const books = [
+  {
+    isbn: '9780394800011',
+    title: 'Dr. Suess',
+    author: 'The Cat in the Hat',
+  },
+  {
+    isbn: '9780808524519',
+    title: 'Green Eggs and Ham',
+    author: '',
+  },
+  {
+    isbn: '',
+    title: '',
+    author: '',
+  },
+];
+console.log(books);
