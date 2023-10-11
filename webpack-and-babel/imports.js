0f4f214765e5960b59ea248e569628c41496d132
Tyler Stahl <tstahl488@gmail.com>
2023-10-11 19:31:40 +0000

    fix file paths

--- webpack-and-babel/imports.js
+++ webpack-and-babel/imports.js
@@ -8,15 +8,15 @@ import f1 from '/foo/bar/baz/qux';
 import f2 from './foo';
 
 // import from a file named `bar.js` located in the parent directory:
-import f3 from './../bar.js';
+import f3 from '../bar';
 
 // import from a file named `index.js` located in a directory named `baz` that
 // is in the current directory:
-import f4 from './baz/index';
+import f4 from './baz';
 
 // import from a file named `index.js` located in a directory named `baz` that
 // is in the parent directory:
-import f5 from './../baz/index';
+import f5 from '../baz';
 
 // import from an `npm` module named `react`:
-import react from 'm';
+import f6 from 'react';
