4533fb7cfd5f0b104c7df8d858eaa1705531a5d3
Tyler Stahl <tstahl488@gmail.com>
2023-10-17 23:35:26 +0000

    await.js section

--- es6-async-await/await.js
+++ es6-async-await/await.js
@@ -5,38 +5,55 @@ import read from './read.js';
 const startTime = Date.now();
 const elapsed = () => `${Math.round((Date.now() - startTime) / 1000)}s -`;
 
-function readOnce() {
-  return read('foo/bar.html').then((msg) =>
-    console.log(elapsed(), 'readOnce:', msg)
-  );
+async function readOnce() {
+  const v = await read('foo/bar.html');
+  console.log(elapsed(), 'readonce: ', v);
+  // return read('foo/bar.html').then((msg) =>
+  //   console.log(elapsed(), 'readOnce:', msg)
+  // );
 }
 
-function readSeveral() {
-  return read('foo1/bar.html')
-    .then((msg) => {
-      console.log(elapsed(), 'readSeveral1:', msg);
-      return read('foo2/bar.html');
-    })
-    .then((msg) => {
-      console.log(elapsed(), 'readSeveral2:', msg);
-      return read('foo3/bar.html');
-    })
-    .then((msg) => console.log(elapsed(), 'readSeveral3:', msg));
+async function readSeveral() {
+  const v = await read('foo1/bar.html');
+  console.log(elapsed(), 'readSeveral1: ', v);
+  const b = await read('foo2/bar.html');
+  console.log(elapsed(), 'readSeveral2: ', b);
+  const n = await read('foo3/bar.html');
+  console.log(elapsed(), 'readSeveral3: ', n);
+  // return read('foo1/bar.html')
+  //   .then((msg) => {
+  //     console.log(elapsed(), 'readSeveral1:', msg);
+  //     return read('foo2/bar.html');
+  //   })
+  //   .then((msg) => {
+  //     console.log(elapsed(), 'readSeveral2:', msg);
+  //     return read('foo3/bar.html');
+  //   })
+  //   .then((msg) => console.log(elapsed(), 'readSeveral3:', msg));
 }
 
-function readChained() {
-  return read('foo-chain/bar.html')
-    .then((msg1) => {
-      console.log(elapsed(), 'readChained1:', msg1);
-      return read(msg1);
-    })
-    .then((msg2) => {
-      console.log(elapsed(), 'readChained2:', msg2);
-      return read(msg2);
-    })
-    .then((msg3) => console.log(elapsed(), 'readChained3:', msg3));
+async function readChained() {
+  const a = await read('foo-chain/bar.html');
+  console.log(elapsed(), 'readchained1: ', a);
+
+  const b = await read(a);
+  console.log(elapsed(), 'readchained2: ', b);
+
+  const c = await read(b);
+  console.log(elapsed(), 'readChained3: ', c);
+
+  // return read('foo-chain/bar.html')
+  //   .then((msg1) => {
+  //     console.log(elapsed(), 'readChained1:', msg1);
+  //     return read(msg1);
+  //   })
+  //   .then((msg2) => {
+  //     console.log(elapsed(), 'readChained2:', msg2);
+  //     return read(msg2);
+  //   })
+  //   .then((msg3) => console.log(elapsed(), 'readChained3:', msg3));
 }
 
-readOnce()
-  .then(() => readSeveral())
-  .then(() => readChained());
+await readOnce();
+await readSeveral();
+await readChained();
