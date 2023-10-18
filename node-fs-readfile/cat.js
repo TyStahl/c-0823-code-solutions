f99bb83b5777ef81223e18e7d327748caa7f6335
Tyler Stahl <tstahl488@gmail.com>
2023-10-18 19:16:13 +0000

    cat.js optimal solution

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,17 +1,31 @@
-import fs from 'node:fs/promises';
+// import fs from 'node:fs/promises';
 
-const [, , ...args] = process.argv;
-console.log(args);
+// const [, , ...args] = process.argv;
+// console.log(args);
 
-async function readAny() {
-  for (let i = 0; i < args.length; i++) {
-    const contents = await fs.readFile(args[i], { encoding: 'utf8' });
-    console.log(contents);
-  }
-}
+// async function readAny() {
+//   for (let i = 0; i < args.length; i++) {
+//     const contents = await fs.readFile(args[i], { encoding: 'utf8' });
+//     console.log(contents);
+//   }
+// }
+
+// try {
+//   await readAny();
+// } catch (e) {
+//   console.log('error: ', e);
+// }
+
+import { readFile } from 'node:fs/promises';
+
+const filenames = process.argv.slice(2);
 
 try {
-  await readAny();
-} catch (e) {
-  console.log('error: ', e);
+  const ps = filenames.map(
+    async (filename) => await readFile(filename, 'utf8')
+  );
+  const contents = await Promise.all(ps);
+  console.log(contents.join('\n'));
+} catch (err) {
+  console.log(err);
 }
