249c36131c5b5e49f14e439b8b4103d99365f50a
Tyler Stahl <tstahl488@gmail.com>
2023-10-19 01:09:55 +0000

    backup

--- node-notes-cli/main.js
+++ node-notes-cli/main.js
@@ -2,16 +2,12 @@ import { readFile, writeFile } from 'node:fs/promises';
 
 async function readNotes() {
   const JSONdata = await readFile('data.json', 'utf8');
-  const dataModel = await JSON.parse(JSONdata);
-  console.log(dataModel);
+  await JSON.parse(JSONdata);
 }
 
 async function writeNew() {
   const noteContent = process.argv.slice(2).join(' ');
   await writeFile('newest-note.txt', noteContent, 'utf8');
-}
-
-async function addtoNotes() {
   const newestNote = await readFile('newest-note.txt', 'utf8');
   let JSONdata = await readFile('data.json', 'utf8');
   const dataModel = await JSON.parse(JSONdata);
@@ -19,13 +15,12 @@ async function addtoNotes() {
   dataModel.nextId++;
   JSONdata = await JSON.stringify(dataModel, null, 2);
   await writeFile('data.json', JSONdata, 'utf8');
-  await readNotes();
+  console.log(JSONdata);
 }
 
 try {
   await readNotes();
   await writeNew();
-  await addtoNotes();
 } catch (error) {
   console.error(error);
   process.exit(1);
