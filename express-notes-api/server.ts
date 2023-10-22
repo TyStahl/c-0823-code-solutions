eac36029d71a877b5c16fd15bb22e4e7b650f06f
Tyler Stahl <tstahl488@gmail.com>
2023-10-22 22:27:48 +0000

    express-notes-api read notes get mdw

--- express-notes-api/server.ts
+++ express-notes-api/server.ts
@@ -1,17 +1,40 @@
 import express from 'express';
+import { readFile } from 'node:fs/promises';
 
 const app = express();
 app.use(express.json());
+app.listen(8080, () => console.log('app is listening on port 8080'));
+
+// type Notes = {
+//   id: number;
+//   content: string;
+// };
+
+const data: any = {};
+console.log(data);
+
+async function readData() {
+  const rData = await readFile('data.json', 'utf-8');
+  return JSON.parse(rData);
+}
+
+// async function writeData(data: any) {
+//   await writeFile('data.json', JSON.stringify(data, null, 2));
+// }
 
 app.get('/api/notes/:id', (req, res) => {
-  // const notesArray: any = [];
-  // for (const key in notes) {
-  //   notesArray.push(notes[key]);
-  // }
-  console.log('user can get list of notes');
-  console.log('user can get a single note by id');
+  if (req.path === '/api/notes') {
+    const notes: any = readData();
+    res.status(200).send(notes);
+    return;
+  }
+  if (req.path === '/api/notes/:id') {
+    const notes: any = readData();
+    const notesArr: any = [];
+    for (const key in notes) {
+      notesArr.push(notes[key]);
+    }
+  }
   console.log(req.method);
   res.json('data.json');
 });
-
-app.listen(8080, () => console.log('app is listening on port 8080'));
