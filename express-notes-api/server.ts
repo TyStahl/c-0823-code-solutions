769dd01858617c27ee45ebb74105b9446fb29960
Tyler Stahl <tstahl488@gmail.com>
2023-10-22 14:57:12 +0000

    express-notes-api

--- express-notes-api/server.ts
+++ express-notes-api/server.ts
@@ -2,17 +2,16 @@ import express from 'express';
 
 const app = express();
 app.use(express.json());
-// const notes = JSON.parse('data.json');
 
-app.get('api/notes', (req, res) => {
-  console.log(req);
-  console.log(res);
-  // // console.log(notes);
+app.get('/api/notes/:id', (req, res) => {
   // const notesArray: any = [];
   // for (const key in notes) {
   //   notesArray.push(notes[key]);
   // }
-  // res.json(notesArray);
+  console.log('user can get list of notes');
+  console.log('user can get a single note by id');
+  console.log(req.method);
+  res.json('data.json');
 });
 
 app.listen(8080, () => console.log('app is listening on port 8080'));
