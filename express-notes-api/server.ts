0f50ceeb073f3a9135284b59083227cc09494d5f
Tyler Stahl <tstahl488@gmail.com>
2023-10-23 16:45:30 +0000

    express-notes-api

--- express-notes-api/server.ts
+++ express-notes-api/server.ts
@@ -5,36 +5,34 @@ const app = express();
 app.use(express.json());
 app.listen(8080, () => console.log('app is listening on port 8080'));
 
-// type Notes = {
-//   id: number;
-//   content: string;
-// };
-
-const data: any = {};
-console.log(data);
+// const data: any = {};
+// console.log(data);
 
 async function readData() {
   const rData = await readFile('data.json', 'utf-8');
-  return JSON.parse(rData);
+  const data = JSON.parse(rData);
+  return data;
 }
 
 // async function writeData(data: any) {
 //   await writeFile('data.json', JSON.stringify(data, null, 2));
 // }
 
-app.get('/api/notes/:id', (req, res) => {
-  if (req.path === '/api/notes') {
-    const notes: any = readData();
-    res.status(200).send(notes);
-    return;
-  }
-  if (req.path === '/api/notes/:id') {
-    const notes: any = readData();
-    const notesArr: any = [];
-    for (const key in notes) {
-      notesArr.push(notes[key]);
-    }
-  }
+app.get('/api/notes', async (req, res) => {
   console.log(req.method);
-  res.json('data.json');
+  const notesArr = [];
+  const journal = await readData();
+  for (const key in journal.notes) {
+    notesArr.push(journal.notes[key]);
+  }
+  console.log('journal', journal);
+  console.log('notesArr', notesArr);
+  res.status(200).json(notesArr);
 });
+
+// app.get('/api/notes/:id', (req, res) => {
+//   console.log(req.method);
+//   const notes: any = readData();
+//   const id: any = req.params.id;
+//   res.status(200).json(notes[id]);
+// });
