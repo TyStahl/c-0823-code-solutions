e54b6215c5d415d5e7fac673f6a9a32d704e178d
Tyler Stahl <tstahl488@gmail.com>
2023-10-29 16:03:27 +0000

    react form controls

--- react-form-controls/my-app/src/RegistrationFormUnControlled.tsx
+++ react-form-controls/my-app/src/RegistrationFormUnControlled.tsx
@@ -1,24 +1,26 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-export function RegistrationFormUncontrolled() {
-  const { formState, setformState } = useState();
-  Event.preventDefault();
+// export function RegistrationFormUncontrolled() {
+//   const { formState, setformState } = useState();
 
-  function handleSubmit() {
-    setformState(FormData);
-    console.log('formData: ', formState);
-  }
-  return (
-    <>
-      <form onSubmit={() => handleSubmit()}>
-        <label>
-          Username: <input></input>
-        </label>
-        <label>
-          Password: <input></input>
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    </>
-  );
-}
+//   function handleSubmit(e: KeyboardEvent) {
+//     e.preventDefault();
+//     const form: null | undefined | HTMLFormElement = e.target;
+//     const formContent = new FormData(form);
+//     setformState(Object.fromEntries(formContent.entries()));
+//     console.log(formState);
+//   }
+//   return (
+//     <>
+//       <form onSubmit={() => handleSubmit}>
+//         <label>
+//           Username: <input defaultValue="username"></input>
+//         </label>
+//         <label>
+//           Password: <input defaultValue="password"></input>
+//         </label>
+//         <button type="submit">Submit</button>
+//       </form>
+//     </>
+//   );
+// }
