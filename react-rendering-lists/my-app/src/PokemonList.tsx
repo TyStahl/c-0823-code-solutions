eb3dc4a23ac7ca7b065ac2426f25b504e50981b8
Tyler Stahl <tstahl488@gmail.com>
2023-10-30 20:39:01 +0000

    react-rendering-lists adjusted passing pokemon array as a prop to ul componenent

--- react-rendering-lists/my-app/src/PokemonList.tsx
+++ react-rendering-lists/my-app/src/PokemonList.tsx
@@ -1,19 +1,10 @@
+type PokemonProps = { pokedex: Pokemon[] };
 type Pokemon = {
   number: string;
   name: string;
 };
 
-type Pokedex = Pokemon[];
-
-const pokedex: Pokedex = [
-  { number: '001', name: 'Bulbasaur' },
-  { number: '004', name: 'Charmander' },
-  { number: '007', name: 'Squirtle' },
-  { number: '025', name: 'Pikachu' },
-  { number: '039', name: 'Jigglypuff' },
-];
-
-export function PokemonList() {
+export function PokemonList({ pokedex }: PokemonProps) {
   const pokemonList = pokedex.map((pokemon: Pokemon) => (
     <li key={pokemon.number}>{pokemon.name}</li>
   ));
