dde8b8869353c455b006b25e3ca057fe2d5ff5ed
Tyler Stahl <tstahl488@gmail.com>
2023-10-30 15:09:32 +0000

    react-validated-input-component

---
+++ react-validated-input-component/my-app/src/ValidatedInput.tsx
@@ -0,0 +1,64 @@
+// import { FaCheck, FaWordpress } from 'react-icons/fa';
+// import { FaTimes } from 'react-icons/fa';
+
+// export function ValidatedInput(e) {
+//   const [password, setPassword] = useState('');
+//   function validatePassword() {
+//     setPassword(e.target.value);
+//     password.toLowercase();
+//     if (
+//       password.includes(
+//         'a' ||
+//           'b' ||
+//           'c' ||
+//           'd' ||
+//           'e' ||
+//           'f' ||
+//           'g' ||
+//           'h' ||
+//           'i' ||
+//           'j' ||
+//           'k' ||
+//           'l' ||
+//           'm' ||
+//           'n' ||
+//           'o' ||
+//           'p' ||
+//           'q' ||
+//           'r' ||
+//           's' ||
+//           't' ||
+//           'u' ||
+//           'v' ||
+//           'w' ||
+//           'x' ||
+//           'y' ||
+//           'z'
+//       ) &&
+//       password.includes(0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)
+//     ) {
+//     }
+//   }
+
+//   return (
+//     <>
+//       <form>
+//         <label>
+//           Username:
+//           <input></input>
+//         </label>
+//         <label>
+//           Password:
+//           <input onChange={validatePassword}></input>
+//         </label>
+//         <button type="submit">Submit</button>
+//         <span>
+//           <FaCheck />
+//         </span>
+//         <span>
+//           <FaTimes />
+//         </span>
+//       </form>
+//     </>
+//   );
+// }
