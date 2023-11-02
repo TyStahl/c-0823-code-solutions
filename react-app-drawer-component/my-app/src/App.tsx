c132a214c8e9b4af8bbc47d7541df73e98632a13
Tyler Stahl <tstahl488@gmail.com>
2023-11-02 01:23:21 +0000

    completed react-app-drawer-component exercise.

---
+++ react-app-drawer-component/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import { AppDrawer } from './AppDrawer.tsx';
+
+const menuItems = ['About', 'Get Started', 'Sign In'];
+
+function App() {
+  return (
+    <>
+      <AppDrawer items={menuItems} />
+    </>
+  );
+}
+
+export default App;
