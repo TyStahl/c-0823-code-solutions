982824524a83b91a266a303b3ee50795d9899f0d
Tyler Stahl <tstahl488@gmail.com>
2023-11-04 20:53:48 +0000

    react-carousel-component install define comp function

---
+++ react-carousel-component/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import { CarouselComponent } from './CarouselComponent';
+
+function App() {
+  return (
+    <>
+      <CarouselComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ react-carousel-component/my-app/src/CarouselComponent.tsx
@@ -0,0 +1,3 @@
+export function CarouselComponent() {
+  return <div>Carousel Component</div>;
+}
