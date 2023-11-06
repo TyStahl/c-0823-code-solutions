5bc04cac06a11b64ff4283dfcbfaf3bd087bc05e
Tyler Stahl <tstahl488@gmail.com>
2023-11-06 18:29:09 +0000

    completed fetch exercise.

---
+++ fetch/main.js
@@ -0,0 +1,28 @@
+async function fetchUsers() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/user');
+    if (!response.ok) {
+      throw new Error(`response not ok ${response.status}`);
+    }
+    const users = await response.json();
+    console.log(users);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function fetchPokemon() {
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/214');
+    if (!response.ok) {
+      throw new Error(`response not ok ${response.status}`);
+    }
+    const favPoke = await response.json();
+    console.log(favPoke);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+fetchUsers();
+fetchPokemon();
