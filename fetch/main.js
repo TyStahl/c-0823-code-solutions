7d9d169d0c9cce417b1124f589f46f2feb29fe01
Tyler Stahl <tstahl488@gmail.com>
2023-11-06 18:30:19 +0000

    fixed intentional error

--- fetch/main.js
+++ fetch/main.js
@@ -1,6 +1,6 @@
 async function fetchUsers() {
   try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/user');
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
     if (!response.ok) {
       throw new Error(`response not ok ${response.status}`);
     }
